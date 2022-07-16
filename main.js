e6f7b0f97f2957f06ab68c6f82d94d53500b13c2
Lucas  de Oliveira Neves <lucasdeoliveiraneves@Mac-mini-de-Lucas.local>
2022-07-16 11:42:16 -0300

    Verificando escolhas dos jogadores

--- main.js
+++ main.js
@@ -13,14 +13,42 @@ const options = [
   },
 ];
 
+let score = {
+  player: 0,
+  computer: 0
+}
+
+let userChoice;
+let computerChoice;
+
 function randomChoiceComputer() {
   const choice = Math.floor(Math.random() * options.length);
   return options[choice].value;
 }
 
+function checkPlayWinner() {
+  const user = options.find(choice => {
+    return choice.value === userChoice
+  })
+
+  if (user.value === computerChoice) {
+    alert('foi um empate!')
+  } else if(user.ganha !== computerChoice) {
+    alert('computador ganhou a rodada')
+  } else {
+    alert('player 1 ganhou')
+    score.player += 1;
+  }
+
+  console.log('user Choice', user)
+  console.log('computer', computerChoice)
+}
+
 document.querySelectorAll(".chose-weapon").forEach((el) =>
   el.addEventListener("click", function (e) {
-    console.log("eu escolho", e.target.textContent);
-    console.log("escolha do computador", randomChoiceComputer());
+    userChoice = e.target.textContent;
+    computerChoice = randomChoiceComputer();
+    checkPlayWinner()
+    console.log(score)
   })
 );
